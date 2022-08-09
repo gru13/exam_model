0086affdf2cafd5d9d44e9f5fb3d56160b9cebdf
gru13 <guruprasath1302@gmail.com>
2022-08-09 15:48:47 +0530

    second frame aglined bbut failed Button command
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- first_try.py
+++ first_try.py
@@ -17,6 +17,9 @@ Database={
 def cur_time(x:Label):
     x.config(text=strftime('%I:%M:%S %p'))
     x.after(1000,lambda :cur_time(x))
+def qnos_command(cur_pos,subject,qno):
+    print(cur_pos,subject,qno)
+
 # ------------------------------------------
 # ------------------main_code---------------
 #  (env-> global,ext->Tk{main window})
@@ -56,9 +59,9 @@ second_frame_Notebook = Notebook(second_frame)
 second_frame_Notebook.pack(fill=BOTH)
 # Creating tabs
 for a in Database:
-    print(a)
-    print(Database[a])
-    print(Database[a][0])
+    # print(a)
+    # print(Database[a])
+    # print(Database[a][0])
     Database[a][0]=Database[a][0](second_frame_Notebook)
     Database[a][0].pack()
     second_frame_Notebook.add(Database[a][0],text=a)
@@ -66,18 +69,38 @@ for a in Database:
     for i in range(no_of_question_len):
         Database[a][-1].append(Button)
     Database[a][-1].append(1)
-    print(Database[a][-1])
+    # print(Database[a][-1])
 # creating qnos 
 for a in Database:
-    print(a)
-    print(Database[a])
+    # print(a)
+    # print(Database[a])
     no_of_question_len = len(Database[a]) - 3 #-3 because of 3 frame
-    print(no_of_question_len)
+    # print(no_of_question_len)
     Database[a][-1][0] = Database[a][-1][0](Database[a][0])
     Database[a][-1][0].pack(side=LEFT) # question nos tab frame
     # qnosf is Database[a][-1][0]
     for ax in range(1,no_of_question_len+1):
-        Database[a][-1][ax] = Database[a][-1][ax](Database[a][-1][0],text=str(ax))
-        Database[a][-1][ax].pack()
+        Database[a][-1][ax] = Database[a][-1][ax](Database[a][-1][0],text=str(ax),command=lambda:qnos_command(Database[a][-1][-1],a,ax))
+    # align of qnos
+    # print(Database[a])
+    # Button are in region of Database[a][-1][1:-1]
+    l_contain_b_index = []
+    if no_of_question_len % 3 == 0:
+        for aa in range(0,no_of_question_len,3):
+            for b in range(0,3):
+                # print(a,b)
+                l_contain_b_index.append([aa,b])
+    else:
+        remain = no_of_question_len % 3
+        for aa in range(int((no_of_question_len-remain)/3)):
+            for b in range(0,3):
+                l_contain_b_index.append([aa,b])
+        # print(remain)
+        for ad in range(remain):
+            l_contain_b_index.append([int((no_of_question_len-remain)/3),ad])
+    print(l_contain_b_index)
+    for ad in range(1,len(l_contain_b_index)+1):
+        Database[a][-1][ad].grid(row=l_contain_b_index[ad-1][0],column=l_contain_b_index[ad-1][1])
+
 # --------------------------------------------
 Win.mainloop()
